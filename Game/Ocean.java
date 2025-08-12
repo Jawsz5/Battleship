41950d9d4c13e282e81140c1e2f896d902c3cb3a
mouryavem <mouryavemuganti@icloud.com>
2025-08-12 18:20:31 -0400

    Finished addBoats and PlaceBoats method

--- Game/Ocean.java
+++ Game/Ocean.java
@@ -1,9 +1,11 @@
 package Game;
+import java.util.ArrayList;
 
 public class Ocean{
     protected int row;
     protected int col;
     protected int[][] grid;
+    private ArrayList<String> Boats = new ArrayList<>();
 
     public Ocean(int row, int col)throws Exception {
         if(row <= 0 || col <= 0) {
@@ -20,6 +22,37 @@ public class Ocean{
                 for(int j = 0; j < col; j++) {
                     grid[i][j] = 0; // Initialize the grid with zeros
                 }
+            }
+
+        }
+    }
+    public addBoats(int row, int col, Boolean orientation, String size)throws Exception{
+        createBoat(row, col, orientation, size);
+        int[] boatSpotx = getSpotsX();
+        int[] boatSpoty = getSpotsY();
+        for(int i = 0 i < boatSpotx.length; i++){
+            if(boatSpotx[i] < row && boatSpoty[i] < col && boatSpotx[i] >= 0 && boatSpoty[i] >= 0){
+                if(grid[boatSpotx[i]][boatSpoty[i]] == 0){
+                    grid[boatSpotx[i]][boatSpoty[i]] = 1; // Mark the boat's position
+                }else {
+                    throw new Exception("Boat overlaps with another boat or is out of bounds.");
+                }
+
+            }else{
+                throw new Exception("Boat is out of bounds.");
+            }
+        
+    }
+    Boats.add(size);
+    
+}
+                
+    public void PlaceBoats(int row, int col, Boolean orientation){
+        for(int i = 0; i < 5; i++){
+            try {
+                addBoats(row, col, orientation, Boats.get(i));
+            } catch (Exception e) {
+                System.out.println("Error placing boat");
             }
 
         }
