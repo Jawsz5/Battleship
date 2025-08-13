f98a8c20b1f776eae7443882095376aecb6f9e92
Jazil <67172230+Jawsz5@users.noreply.github.com>
2025-08-13 18:21:19 -0400

    exception handling for ship

--- app/src/main/Game/Ship.java
+++ app/src/main/Game/Ship.java
@@ -1,12 +1,21 @@
 package app.src.main.Game;
 
+import java.io.IOException;
+
 public class Ship{
     private int size, startx, starty;
     private Boolean isVertical, isAHit;
     private int[] spotsX, spotsY;
     //initialize ship position
      //overload the constructor to allow different kinds of input
-     public Ship(int x, int y, Boolean vertical, int s){setXY(x,y); setDirection(vertical); setSize(s); 
+     public Ship(int x, int y, Boolean vertical, int s){
+        try{setXY(x,y);}catch(IllegalArgumentException e){System.out.println("coordinates of the boat must be integers");}
+        try{setDirection(vertical);}catch(IllegalArgumentException e){
+            System.out.println("direction of the boat must be either a boolean or string");
+        }
+        try{setSize(s);}catch(IllegalArgumentException e){
+            System.out.println("size of the boat must be an integer or valid string");
+        }
         spotsX = new int[size];
         spotsY = new int[size];
         for(int i = 0; i < spotsX.length; i++){
@@ -23,16 +32,37 @@ public class Ship{
         setDirection(vertical);setSize(s); new Ship(x, y, isVertical, size);
     }
     public Ship(int x, int y, Boolean vertical, String s){
-        setDirection(vertical);setSize(s); new Ship(x, y, isVertical, size);
+        setDirection(vertical); 
+        try{setSize(s);}catch(IllegalArgumentException | IOException e){
+            System.out.println("size of the boat must be an integer or valid string. If the input isn't a valid string, check the IO exception");
+        }
+        new Ship(x, y, isVertical, size);
     }
     public Ship(int x, int y, String vertical, String s){
-        setDirection(vertical);setSize(s); new Ship(x, y, isVertical, size);
+        setDirection(vertical);
+        try{setSize(s);}catch(IllegalArgumentException | IOException e){
+            System.out.println("size of the boat must be an integer or valid string. If the input isn't a valid string, check the IO exception");
+        }
+        new Ship(x, y, isVertical, size);
     }
 
-    private void setXY(int x, int y){startx = x; starty = y;}
+    private void setXY(int x, int y)throws IllegalArgumentException{
+        if(x != (int)x){
+            throw new IllegalArgumentException("coordinates of the boat must be integers");
+        }
+        startx = x; starty = y;
+    }
     //2 methods for direction and size setting for simplicity later on
-    private void setSize(int s){size = s;}
-    private void setSize(String s){
+    private void setSize(int s) throws IllegalArgumentException{
+        if (s != (int)s){
+            throw new IllegalArgumentException("size of the boat must be an integer or valid string");
+        }
+        size = s;
+    }
+    private void setSize(String s) throws IllegalArgumentException, IOException{
+        if (!(s instanceof String)){
+            throw new IllegalArgumentException("size of the boat must be either an integer or valid string");
+        }
         s = s.toLowerCase().strip();
         String[][] sizes = new String[][]{
             {"aircraft", "aircraftcarrier", "a", "air"}, //due to strip function, aircraft carrier is one word
@@ -46,11 +76,15 @@ public class Ship{
                 if(m.equals(s)){size = i;}
             }
         }
+        if(size == 0){throw new IOException("size input is not contained in the string list not is it a valid integer");}
         //map the position in the sizes array to actual ship size
         int[] size_map = new int[]{5, 4, 3, 3, 2};
         size = size_map[size];
     }
-    private void setDirection(String vert){
+    private void setDirection(String vert) throws IllegalArgumentException{
+        if (!(vert instanceof String)){
+            throw new IllegalArgumentException("direction of the boat must be either a boolean or string");
+        }
         vert = vert.toLowerCase().strip();
         String[] possible_vert_strings = new String[]{"vertical", "v", "vert"};
         for(String i: possible_vert_strings){
@@ -60,7 +94,10 @@ public class Ship{
         }
         isVertical = false;
     }
-    private void setDirection(Boolean vert){
+    private void setDirection(Boolean vert) throws IllegalArgumentException{
+        if (!(vert instanceof Boolean)){
+            throw new IllegalArgumentException("direction of the boat must be either a boolean or string");
+        }
         if(vert == true){isVertical = true;}
         else{isVertical = false;}
     }
@@ -71,11 +108,17 @@ public class Ship{
     public int getX(){return startx;}
     public int getY(){return starty;}
     //hit tracking
-    public Boolean isHit(int x, int y){
+    public Boolean isHit(int x, int y) throws IllegalArgumentException{
+        if(x != (int)x || y != (int)y){
+            throw new IllegalArgumentException("Input coordinates must be integers");
+        }
         hit(x,y);
         return isAHit;
     }
-    public void hit(int x, int y){
+    public void hit(int x, int y) throws IllegalArgumentException{
+        if(x != (int)x || y != (int)y){
+            throw new IllegalArgumentException("Input coordinates must be integers");
+        }
         // if the x and y coordinates fall on the boat
         Boolean hitX = false;
         Boolean hitY = false;
---
+++ app/src/test/java/org/example/ShipTests.java
@@ -0,0 +1,11 @@
+package app.src.test.java.org.example;
+import org.junit.jupiter.api.Test;
+
+public class ShipTests {
+    @Test 
+    void shipWorks(){
+        
+    }
+
+
+}
