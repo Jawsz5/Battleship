930b4cb289ee5a40534548468fb078fa562b231a
Jazil <67172230+Jawsz5@users.noreply.github.com>
2025-09-06 17:13:55 -0400

    fixing up more issues

--- src/test/java/OceanTest.java
+++ src/test/java/OceanTest.java
@@ -1,3 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
 class OceanTest{
 
     //constructor tests
--- src/test/java/ShipTest.java
+++ src/test/java/ShipTest.java
@@ -1,3 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ShipTest {
    @Test
    @DisplayName("Boolean+String ctor: vertical placement builds spots along +Y")
